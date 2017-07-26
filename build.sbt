068d149480571ba862c73245f89e92b3efa06a47
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-07-26 19:29:01 +0900

    add actor

--- build.sbt
+++ build.sbt
@@ -2,5 +2,11 @@ name := "webapp-curriculum-3009"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
+scalaVersion := "2.12.2"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.3",
+  "com.typesafe.akka" %% "akka-testkit" % "2.5.3" % Test
+)
+
         
--- src/main/scala/MustResume.scala
+++ src/main/scala/MustResume.scala
@@ -1,3 +1,17 @@
+import akka.actor.{Actor, Props}
+
+class ParentActor extends Actor {
+  def receive = {
+    case p: Props => sender() ! context.actorOf(p)
+  }
+}
+
+class ChildActor extends Actor {
+  def receive = {
+    case e: Exception => throw e
+  }
+}
+
 object MustResume extends App {
 
 }
